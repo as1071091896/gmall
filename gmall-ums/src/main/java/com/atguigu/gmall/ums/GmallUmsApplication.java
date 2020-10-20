b39a44af4800c72cebcd7a4c573bf5e53d89676d
as1071091896 <1071091896@qq.com>
2020-10-20 13:53:24 +0800

    逆向生成代码

--- gmall-ums/src/main/java/com/atguigu/gmall/ums/GmallUmsApplication.java
+++ gmall-ums/src/main/java/com/atguigu/gmall/ums/GmallUmsApplication.java
@@ -1,13 +1,13 @@
-package com.zhao.gmall.pms;
+package com.atguigu.gmall.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GmallPmsApplication {
+public class GmallSmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GmallPmsApplication.class, args);
+		SpringApplication.run(GmallSmsApplication.class, args);
 	}
 
 }
